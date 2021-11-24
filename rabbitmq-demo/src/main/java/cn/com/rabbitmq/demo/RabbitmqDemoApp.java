8da642be489dba78b0da2b45b1913c8e77d1d0b8
zhangguangming <4995869@qq.com>
2021-11-24 21:43:39 +0800

    23232323

---
+++ rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/RabbitmqDemoApp.java
@@ -0,0 +1,19 @@
+package cn.com.rabbitmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zgming
+ */
+@SpringBootApplication(scanBasePackages = {
+        "cn.com.common.handle",
+        "cn.com.common.config"
+})
+public class RabbitmqDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqDemoApp.class, args);
+    }
+
+}
