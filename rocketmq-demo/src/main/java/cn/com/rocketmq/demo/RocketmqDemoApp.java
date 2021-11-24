f2836186481b3aa4d524f3b7555afe0694518e7e
zhangguangming <4995869@qq.com>
2021-11-24 13:32:16 +0800

    23232323

--- rocketmq-demo/src/main/java/cn/com/rocketmq/demo/RocketmqDemoApp.java
+++ rocketmq-demo/src/main/java/cn/com/rocketmq/demo/RocketmqDemoApp.java
@@ -1,18 +1,15 @@
 package cn.com.rocketmq.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author zgming
  */
-@MapperScan(basePackages = {
-})
+
 @SpringBootApplication(scanBasePackages = {
         "cn.com.common.handle",
-        "cn.com.common.config",
-        "cn.com.common.service",
+        "cn.com.common.config"
 })
 public class RocketmqDemoApp {
 
