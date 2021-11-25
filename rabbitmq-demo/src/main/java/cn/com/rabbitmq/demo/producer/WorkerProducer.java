a261e34f1e98cf6427d507eb95cc8bc583845ab9
zhangguangming <4995869@qq.com>
2021-11-25 08:46:58 +0800

    23232323

--- rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/producer/WorkerProducer.java
+++ rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/producer/WorkerProducer.java
@@ -1,5 +1,10 @@
 package cn.com.rabbitmq.demo.producer;
 
+import cn.com.rabbitmq.demo.utils.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+
+import java.util.Scanner;
+
 /**
  * 生产者
  *
@@ -8,4 +13,21 @@ package cn.com.rabbitmq.demo.producer;
  * @Date 2021/11/24 23:26
  */
 public class WorkerProducer {
+
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        try (Channel channel = RabbitMqUtils.getChannel();) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            //从控制台当中接受信息
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                String message = scanner.next();
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println("发送消息完成:" + message);
+            }
+        }
+    }
+
+
 }
