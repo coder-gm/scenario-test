ae8eab50a7a5a14b6c6452d338623e2e8860277d
zhangguangming <4995869@qq.com>
2021-11-25 22:40:43 +0800

    23232323

---
+++ rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/consumer/ReceiveLogsDirect01.java
@@ -0,0 +1,9 @@
+package cn.com.rabbitmq.demo.consumer;
+
+/**
+ * @Description
+ * @Author Guang Ming Zhang
+ * @Date 2021/11/25 19:48
+ */
+public class ReceiveLogsDirect01 {
+}
---
+++ rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/consumer/ReceiveLogsDirect02.java
@@ -0,0 +1,9 @@
+package cn.com.rabbitmq.demo.consumer;
+
+/**
+ * @Description
+ * @Author Guang Ming Zhang
+ * @Date 2021/11/25 20:03
+ */
+public class ReceiveLogsDirect02 {
+}
