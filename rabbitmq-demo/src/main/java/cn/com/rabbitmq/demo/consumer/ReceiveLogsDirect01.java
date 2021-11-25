5f4043337c15412680cd2f01359b0c8d6ea08817
zhangguangming <4995869@qq.com>
2021-11-25 22:40:51 +0800

    23232323

--- rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/consumer/ReceiveLogsDirect01.java
+++ rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/consumer/ReceiveLogsDirect01.java
@@ -1,9 +1,40 @@
 package cn.com.rabbitmq.demo.consumer;
 
+import cn.com.rabbitmq.demo.utils.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
 /**
  * @Description
  * @Author Guang Ming Zhang
  * @Date 2021/11/25 19:48
  */
 public class ReceiveLogsDirect01 {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+
+    public static void main(String[] argv) throws Exception {
+        //步骤1.创建信道
+        Channel channel = RabbitMqUtils.getChannel();
+
+        //创建队列
+        String queueName = "disk";
+        channel.queueDeclare(queueName, false, false, false, null);
+
+        //绑定交换机与队列之间的关系
+        channel.queueBind(queueName, EXCHANGE_NAME, "error");
+        System.out.println("等待接收消息.....");
+
+
+        //接收消息
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" 接收绑定键 :"+delivery.getEnvelope().getRoutingKey()+", 消 息:"+message);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+
+
 }
--- rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/consumer/ReceiveLogsDirect02.java
+++ rabbitmq-demo/src/main/java/cn/com/rabbitmq/demo/consumer/ReceiveLogsDirect02.java
@@ -1,9 +1,42 @@
 package cn.com.rabbitmq.demo.consumer;
 
+import cn.com.rabbitmq.demo.utils.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
 /**
  * @Description
  * @Author Guang Ming Zhang
  * @Date 2021/11/25 20:03
  */
 public class ReceiveLogsDirect02 {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+
+    public static void main(String[] argv) throws Exception {
+        //步骤1.创建信道
+        Channel channel = RabbitMqUtils.getChannel();
+
+        //创建队列
+        String queueName = "console";
+        channel.queueDeclare(queueName, false, false, false, null);
+
+        //绑定交换机与队列之间的关系
+        channel.queueBind(queueName, EXCHANGE_NAME, "info");
+        channel.queueBind(queueName, EXCHANGE_NAME, "warning");
+        System.out.println("等待接收消息.....");
+
+        //接收消息
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" 接收绑定键 :"+delivery.getEnvelope().getRoutingKey()+", 消 息:"+message);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+
+
+    }
+
+
 }
