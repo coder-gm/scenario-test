af05616669e2bc69efc2c505b677db471cb2c2e4
zhangguangming <4995869@qq.com>
2021-12-04 16:53:51 +0800

    232323

---
+++ java-basics/src/main/java/cn/com/java/basics/api/util/collection/HashMapThreadNotSafeTest.java
@@ -0,0 +1,57 @@
+package cn.com.java.basics.api.util.collection;
+
+import javax.swing.text.Segment;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * HashSet线程不安全示例
+ *
+ * @Description
+ * @Author Guang Ming Zhang
+ * @Date 2021/12/4 15:01
+ */
+public class HashMapThreadNotSafeTest {
+
+
+    public static void main(String[] args) {
+        HashMapHolder hashMapHolder = new HashMapHolder();
+        hashMapHolder.setName("子线程");
+        hashMapHolder.start();
+
+
+        //在主线程中往list中添加10万个元素
+        for (int i = 0; i < 10_0000; i++) {
+            HashMapHolder.map.put(i, i);
+        }
+
+
+        // 期望10万
+        // 实际大于10万或者小于10万
+        System.out.println(Thread.currentThread().getName() + "HashMap 集合数量是：" + HashMapHolder.map.size());
+
+
+    }
+
+}
+
+
+class HashMapHolder extends Thread {
+    Segment
+
+    static Map<Integer, Integer> map = new ConcurrentHashMap<>(131072);
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10_0000; i++) {
+            map.put(i, i);
+        }
+        System.out.println(Thread.currentThread().getName() + " HashMap元素的数量：" + map.size());
+    }
+
+
+}
