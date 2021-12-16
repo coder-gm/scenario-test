4944e4a2ef70ba83729f855c513dc7c4251fc9b1
zhangguangming <4995869@qq.com>
2021-12-16 16:42:24 +0800

    232323

---
+++ docker-demo/src/main/java/cn/com/docker/DockerDemoApp.java
@@ -0,0 +1,16 @@
+package cn.com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zgming
+ */
+@SpringBootApplication
+public class DockerDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApp.class, args);
+    }
+
+}
---
+++ docker-demo/src/main/java/cn/com/docker/controller/DockerDemoController.java
@@ -0,0 +1,21 @@
+package cn.com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author Guang Ming Zhang
+ * @Date 2021/12/15 22:49
+ */
+@RestController
+@RequestMapping("/docker")
+public class DockerDemoController {
+
+    @GetMapping("/demo")
+    public String dockerDemo() {
+        return "Idea 整合 Docker 功能测试 ！！";
+    }
+
+}
